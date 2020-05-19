79a8b2b2027844b9991e83da4aac393c0683488d
HikariNoHaka <ps1721998@gmail.com>
2020-05-19 12:36:14 +0530

    addint files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>hello world</h1>
+      <Calculator></Calculator>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Calculator from  './components/Calculator.vue'
+  export default{
+      name:'App',
+     components:{
+         Calculator
+     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+      background-color: slategrey;
+  }
+  
 </style>
+
